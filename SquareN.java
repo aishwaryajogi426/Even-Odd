348e7f859189f14098d7824e56f3231a71816752
Aishwarya P Jogi <aishwaryapj426@gmail.com>
2026-02-15 00:30:59 +0530

    Update SquareN.java

--- SquareN.java
+++ SquareN.java
@@ -3,7 +3,9 @@ public class SquareN
     public static int Sum(int sn)
     {
         int sum=0;
+        //Using for loop for natural numbers
         for(int i=1;i<=sn;i++)
+        //squaring and adding the natural numbers    
             sum += (i*i);
         return sum;
     }
@@ -11,5 +13,6 @@ public class SquareN
         int sn=8;
         System.out.println(Sum(sn));
     }
+
 
 }
