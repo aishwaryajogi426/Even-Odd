a2988736de9b4d0fb3a80d26e9916925a1acc393
Aishwarya P Jogi <aishwaryapj426@gmail.com>
2026-02-15 00:32:56 +0530

    Update Table.java

--- Table.java
+++ Table.java
@@ -5,6 +5,8 @@ class Table
 {
     public static void multiply(int b)
     {
+        //using for loop for the number of terms in the table
+    
         for(int i=1;i<11;i++)
         {
             System.out.println(b+"*"+i+"="+b*i);
@@ -14,4 +16,5 @@ class Table
         int b=6;
         multiply(b);
     }
+
 }
